6ad8989de71442feae540f7856960041e47f73c6
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-04-16 20:14:05 -0400

    Trip model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Trip(models.Model):
+    destination = models.CharField(max_length=100)
+    dates = models.CharField(max_length=100)
+    hotel = models.CharField(max_length=100)
+    cover = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
+class Trip:
+    def __init__(self, destination, dates, hotel, hotel_link, img_url):
+        self.destination = destination
+        self.dates = dates
+        self.hotel = hotel
+        self.hotel_link = hotel_link
+        self.img_url = img_url
 
+
+trips = [
+    Trip('Tulum', 'April 1st - April 10th', 'Casa Malca', 'https://www.booking.com', 'https://imgur.com/h3XTFBx'),
+    Trip('Miami', 'May 1st - May 15', 'SLS', 'https://materializecss.com/cards.html', 'https://imgur.com/VdB5uKA'),
+]
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def my_trips(request):
+    return render(request, 'my_trips.html', {'trips': trips})
